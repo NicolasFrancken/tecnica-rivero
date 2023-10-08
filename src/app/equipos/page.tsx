959b7e53fb51736659fa2a5dd7224115675793f6
Nico <franckennicolas@gmail.com>
2023-10-08 13:50:14 -0700

    je

--- src/app/equipos/page.tsx
+++ src/app/equipos/page.tsx
@@ -5,35 +5,45 @@ import { Accordion, AccordionItem } from "@nextui-org/react";
 
 export default function Equipos() {
   const itemClasses = {
+    base: "w-[500px]",
+    content: "w-[500px]",
     title: "font-semibold text-lg md:text-xl xl:text-2xl",
     subtitle: "text-xs md:text-sm xl:text-base",
   };
   return (
     <div>
       <CustomNavbar />
-      <section className="min-h-screen py-16 px-3 md:px-12 2xl:px-96 pt-32 md:pt-40 background3 flex flex-col md:items-center gap-10 md:gap-20">
-        <h2 className="font-bold text-3xl md:text-5xl xl:text-5xl text-center ">
-          Tenemos el equipo perfecto para vos!
-        </h2>
+      <section className="min-h-screen py-16 px-3 md:px-12 2xl:px-96 pt-32 md:pt-32 background3 flex flex-col md:items-center gap-5">
+        <div className="bg-background w-full py-3 rounded-medium">
+          <h2 className="font-bold text-3xl md:text-5xl xl:text-4xl text-center ">
+            Tenemos el equipo perfecto para vos!
+          </h2>
+        </div>
         <Accordion
           variant="shadow"
-          className="text-inherit"
+          className="text-inherit w-[500px]"
           itemClasses={itemClasses}
         >
           <AccordionItem
             key="1"
             title="Fotocopiadoras"
-            subtitle="La mejor opción para empresas chicas y medianas"
+            subtitle="La mejor opción para tu oficina"
             aria-label="Fotocopiadoras"
           >
-            <ul>
-              <li>HOLAAAAAAAA</li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <h4 className="font-semibold text-xl">B&N</h4>
+            <ul className="list-disc">
+              <li>M 320F</li>
+              <li>SP 3710</li>
+              <li>IM 430F</li>
+              <li>MP 305</li>
+              <li>MP 301</li>
+              <li>IM 2500</li>
+            </ul>
+            <h4 className="font-semibold text-xl">Color</h4>
+            <ul className="list-disc">
+              <li>IM C300F</li>
+              <li>IM C2010</li>
+              <li>M C251FW</li>
             </ul>
           </AccordionItem>
           <AccordionItem
@@ -43,13 +53,10 @@ export default function Equipos() {
             aria-label="Impresoras"
           >
             <ul>
-              <li>HOLAAAAAAAA</li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>P 311</li>
+              <li>P 502</li>
+              <li>P C311W</li>
+              <li>SP C840DN</li>
             </ul>
           </AccordionItem>
           <AccordionItem
@@ -59,13 +66,7 @@ export default function Equipos() {
             aria-label="Formato Ancho"
           >
             <ul>
-              <li>HOLAAAAAAAA</li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>MP W6700</li>
             </ul>
           </AccordionItem>
           <AccordionItem
@@ -75,17 +76,16 @@ export default function Equipos() {
             aria-label="Alta Produccíon"
           >
             <ul>
-              <li>HOLAAAAAAAA</li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>MP 6002</li>
+              <li>MP 7503</li>
+              <li>IM 7000</li>
             </ul>
           </AccordionItem>
         </Accordion>
-      </section>
+      <div className="bg-background px-4 py-2 rounded-small">
+          <p className="font-semibold text-lg">Contamos con +100 modelos más! Si buscas un equipo en específico, <a href="mailto:tecnicarivero@gmail.com" className="underline font-bold">constultá acá.</a></p>
+     
+      </div>   </section>
       <Footer />
     </div>
   );
